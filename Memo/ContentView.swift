134904207fe22887652f63ad552ee07183eac418
Edilson Almeida <contato.edilsonalmeida@gmail.com>
2022-09-28 20:52:33 -0300

    Initial Commit

---
+++ Memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by edilsonalmeida on 28/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by edilsonalmeida on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
